33e0bbdb74b9236e75a37165fb033421d1702711
Sai kumar annapureddy <annapureddy.sai.kumar.94@gmail.com>
2017-10-24 20:17:38 -0400

    Using proper indentations

--- 10/solution.java
+++ 10/solution.java
@@ -3,21 +3,16 @@ public class helloworld
 {
 	public static void main(String args[]) throws IOException
 	{
-			int i =0;
+		int i =0;
 	      BigInteger f = new BigInteger("0");
-	      //BigInteger a = new 
 	      while(i++<20_00_000)
 	      {	
-	    	  
 	    	  BigInteger a = BigInteger.valueOf(i);
 	    	  if(a.isProbablePrime(10)){
 	    		  f=f.add(a);  
-	    	  }
-	    	  
-	      }
-	      
-	  System.out.println(f);
-		
+	    	  } 
+	      }  
+	  System.out.println(f);	
 	}
 }
 
