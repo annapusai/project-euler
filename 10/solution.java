91b3f52bdc197f5121ec4fd54c48c8a97afc2cdd
Sai kumar annapureddy <annapureddy.sai.kumar.94@gmail.com>
2017-10-24 20:16:14 -0400

    java 8

---
+++ 10/solution.java
@@ -0,0 +1,25 @@
+import java.math.*;
+public class helloworld 
+{
+	public static void main(String args[]) throws IOException
+	{
+			int i =0;
+	      BigInteger f = new BigInteger("0");
+	      //BigInteger a = new 
+	      while(i++<20_00_000)
+	      {	
+	    	  
+	    	  BigInteger a = BigInteger.valueOf(i);
+	    	  if(a.isProbablePrime(10)){
+	    		  f=f.add(a);  
+	    	  }
+	    	  
+	      }
+	      
+	  System.out.println(f);
+		
+	}
+}
+
+
+
